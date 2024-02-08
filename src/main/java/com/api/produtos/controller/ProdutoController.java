c32865c3fb10b61b1732089e4a45e10e9456caa0
Ernani12 <39600025+Ernani12@users.noreply.github.com>
2024-02-08 18:32:09 -0300

    Add map to  list in Controller to list service

--- src/main/java/com/api/produtos/controller/ProdutoController.java
+++ src/main/java/com/api/produtos/controller/ProdutoController.java
@@ -1,10 +1,25 @@
 package com.api.produtos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.produtos.model.Produto;
+import com.api.produtos.service.ProdutoService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService S;
+
+    @GetMapping("/list")
+    public Iterable<Produto> listar() {
+        return S.listar();
+    }
+    
 
     @GetMapping("/") //mapping default
     public String route(){
