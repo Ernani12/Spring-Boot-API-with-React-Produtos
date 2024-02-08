8a2838cc0af45c610788d292ffcba2bbe9aac5c3
Ernani12 <39600025+Ernani12@users.noreply.github.com>
2024-02-08 18:22:09 -0300

    Add service list all produtcs

--- src/main/java/com/api/produtos/service/ProdutoService.java
+++ src/main/java/com/api/produtos/service/ProdutoService.java
@@ -1,5 +1,19 @@
 package com.api.produtos.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.produtos.model.Produto;
+import com.api.produtos.repository.ProdutoRepository;
+
+@Service
 public class ProdutoService {
     
+    @Autowired
+    private ProdutoRepository pr;
+
+    //list all products
+    public Iterable<Produto> listar(){
+        return pr.findAll();
+    }
 }
